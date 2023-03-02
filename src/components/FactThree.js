7b0b5e04ed5603db9e8230ae32f7daeac78b4760
Douglas <mercurymuur86@yahoo.com>
2023-03-02 05:01:53 -0500

    nike head

--- src/components/FactThree.js
+++ src/components/FactThree.js
@@ -4,8 +4,8 @@ import React from 'react';
     const FactThree = () => {
         return (
             <div id="shoebox3">
-                <h3 className='header3'>Fun Facts about Shoes!!!</h3>
-                <p className='header3'> A pair of 1972 Nike running shoes called the 'Waffle Shoe' became the most expensive sneakers ever sold at auction, selling for a total price at $475,500. </p>
+                <h3 className='header2'>Fun Facts about Shoes!!!</h3>
+                <p className='header2'> A pair of 1972 Nike running shoes called the 'Waffle Shoe' became the most expensive sneakers ever sold at auction, selling for a total price at $475,500. </p>
             </div>
         )
     }
